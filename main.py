ceaee324218b6d7cd92159ab8782671f5288f35c
YamkelaMathontsi <yamkelapj@gmail.com>
2021-06-08 14:50:32 +0200

    Added files

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# Mini Calculator App
+
+class MiniCalculator():
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
